55fcd269ea9c6fdefd11364118f7fdaac7bcc333
dandelion-huang <dandelion820308@gmail.com>
2024-08-18 12:29:26 +0800

    feat: 🎸 (rust-course/single-thread-web-server) 🎉

--- rust/rust-course/single-thread-web-server/src/main.rs
+++ rust/rust-course/single-thread-web-server/src/main.rs
@@ -18,8 +18,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     // the ending of the http request is an empty line
-    let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
-    // println!("Request: {:#?}", http_request);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     // http request:
     // Method Request-URI HTTP-Version
@@ -31,9 +30,14 @@ fn handle_connection(mut stream: TcpStream) {
     // headers CRLF
     // body (GET Method no body)
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 Not Found", "404.html")
+    };
+    let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
+
 }
